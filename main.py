b4f59f303c24ecfe54073c7da247500f40691a72
Paul Kottering <paulkottering@berkeley.edu>
2023-06-27 18:06:55 -0400

    update train plot

--- main.py
+++ main.py
@@ -5,34 +5,64 @@ from stable_baselines.common.evaluation import evaluate_policy
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Initialize the training environment using the shaped reward structure
 env_train = NFLGame_shaped()
-env_test = NFLGame()
+# Initialize the testing environments
+env_test_simple = NFLGame()
+env_test_shaped = NFLGame_shaped()
 
-# Training final model with the best parameters
+# Initialize the PPO2 model
+# The 'MlpPolicy' is a type of policy optimized for PPO,
+# which uses a multilayer perceptron (neural network) for function approximation.
 model = PPO2('MlpPolicy', env_train, verbose=1, cliprange=0.4)
 
-mean_rewards = []
-std_rewards = []
+# Initialize empty lists to store mean rewards and standard deviations
+mean_rewards_simple = []
+std_rewards_simple = []
+mean_rewards_shaped = []
+std_rewards_shaped = []
+
+# Set the number of timesteps for each evaluation period
 eval_timesteps = 10000
-n_eval_episodes = 1000
-total_timesteps = 500000
+# Set the number of episodes to evaluate for each period
+n_eval_episodes = 100
+# Set the total number of training timesteps
+total_timesteps = 50000
+# Set the name of the model for saving
 model_name = "PPO_nfl_shaped_2"
 
+# Train the model for the specified total number of timesteps,
+# evaluating and recording the mean and std reward every eval_timesteps
 for i in range(total_timesteps // eval_timesteps):
     model.learn(total_timesteps=eval_timesteps)
-    mean_reward, std_reward = evaluate_policy(model, env_test, n_eval_episodes=n_eval_episodes)
-    mean_rewards.append(mean_reward)
-    std_rewards.append(std_reward)
-    print('Mean = ',mean_reward)
-    print('Eval Number = ',i)
 
-# Save the agent
+    mean_reward, std_reward = evaluate_policy(model, env_test_simple, n_eval_episodes=n_eval_episodes)
+    mean_rewards_simple.append(mean_reward)
+    std_rewards_simple.append(std_reward)
+
+    mean_reward, std_reward = evaluate_policy(model, env_test_shaped, n_eval_episodes=n_eval_episodes)
+    mean_rewards_shaped.append(mean_reward)
+    std_rewards_shaped.append(std_reward)
+
+    print('Eval Number = ', i)
+
+# Save the trained model
 model.save('models/'+model_name)
 
+# Create a figure with two subplots
+fig, axs = plt.subplots(2)
 
-# Plotting mean rewards with std as error bars
-plt.errorbar(np.arange(len(mean_rewards)) * eval_timesteps, mean_rewards, yerr=std_rewards, fmt='o')
-plt.title('Training progress')
-plt.xlabel('Timesteps')
-plt.ylabel('Mean Reward')
+# Plotting mean rewards with std as error bars for the simple reward environment
+axs[0].errorbar(np.arange(len(mean_rewards_simple)) * eval_timesteps, mean_rewards_simple, yerr=std_rewards_simple, fmt='o')
+axs[0].set_title('Training progress on Simple Reward Environment')
+axs[0].set_xlabel('Timesteps')
+axs[0].set_ylabel('Mean Reward')
+
+# Plotting mean rewards with std as error bars for the shaped reward environment
+axs[1].errorbar(np.arange(len(mean_rewards_shaped)) * eval_timesteps, mean_rewards_shaped, yerr=std_rewards_shaped, fmt='o')
+axs[1].set_title('Training progress on Shaped Reward Environment')
+axs[1].set_xlabel('Timesteps')
+axs[1].set_ylabel('Mean Reward')
+
+# Save the figure
 plt.savefig('plotting/train_plot_'+ model_name + '.png')
